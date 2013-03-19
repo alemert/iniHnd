e4964117933bac26ffed3a0ee6f245b976dff014
Ales Mertuek <ales.mertuek@deutsche-boerse.com>
2013-03-19 21:06:05 +0000

    feierabend
    git-svn-id: http://syspver1/svn/develop/modules/iniHnd@674 72f11a4d-5a25-4e8a-b8c8-85b0af744ad3

--- test/src/t_reader_002.c
+++ test/src/t_reader_002.c
@@ -45,9 +45,9 @@ int main( int argc, const char** argv )
 #if(1)
   iniReader( "test/cfg/t_reader_002_000.ini", &iniMem ) ;
   startVal = iniMem ;
-  doPointTest( "base test "     , \
-               RC_NOT_NULL      , \
-               iniHandleValues  , \
+  doPointTest( "no values found"  , \
+               RC_NOT_NULL        , \
+               iniHandleValues    , \
                startVal, endVal, iniCfg, &sysRc   ) ;
   if( sysRc != -1 )
   {
@@ -57,6 +57,107 @@ int main( int argc, const char** argv )
   }
   checkMessage( TEST_OK_TXT, iniHandleOpenTag ) ;
   free(iniMem) ;
+#endif
+
+  // -------------------------------------------------------
+  // some test 
+  // -------------------------------------------------------
+#if(1)
+  iniReader( "test/cfg/t_reader_002_001.ini", &iniMem ) ;
+  startVal = iniMem ;
+  doPointTest( "no values found"  , \
+               RC_NOT_NULL        , \
+               iniHandleValues    , \
+               startVal, endVal, iniCfg, &sysRc   ) ;
+  if( sysRc != -1 )
+  {
+    checkMessage( TEST_ERR_TXT, iniHandleOpenTag ) ;
+    sysRc = 1 ;
+    goto _door ;
+  }
+  checkMessage( TEST_OK_TXT, iniHandleOpenTag ) ;
+  free(iniMem) ;
+#endif
+
+  // -------------------------------------------------------
+  // some test 
+  // -------------------------------------------------------
+#if(1)
+  iniReader( "test/cfg/t_reader_002_002.ini", &iniMem ) ;
+  startVal = iniMem ;
+  doPointTest( "early > "         , \
+               RC_IS_NULL        , \
+               iniHandleValues    , \
+               startVal, endVal, iniCfg, &sysRc   ) ;
+  if( sysRc != 1 )
+  {
+    checkMessage( TEST_ERR_TXT, iniHandleOpenTag ) ;
+    sysRc = 1 ;
+    goto _door ;
+  }
+  checkMessage( TEST_OK_TXT, iniHandleOpenTag ) ;
+  free(iniMem) ;
+#endif
+
+  // -------------------------------------------------------
+  // some test
+  // -------------------------------------------------------
+#if(1)
+  iniReader( "test/cfg/t_reader_002_003.ini", &iniMem ) ;
+  startVal = iniMem ;
+  doPointTest( "early eof "         , \
+               RC_IS_NULL           , \
+               iniHandleValues    , \
+               startVal, endVal, iniCfg, &sysRc   ) ;
+  if( sysRc != 2 )
+  {
+    checkMessage( TEST_ERR_TXT, iniHandleOpenTag ) ;
+    sysRc = 2 ;
+    goto _door ;
+  }
+  checkMessage( TEST_OK_TXT, iniHandleOpenTag ) ;
+  free(iniMem) ;
+#endif
+
+  // -------------------------------------------------------
+  // some test
+  // -------------------------------------------------------
+#if(1)
+  iniReader( "test/cfg/t_reader_002_004.ini", &iniMem ) ;
+  startVal = iniMem ;
+  doPointTest( "early = "         , \
+               RC_IS_NULL           , \
+               iniHandleValues    , \
+               startVal, endVal, iniCfg, &sysRc   ) ;
+  if( sysRc != 3 )
+  {
+    checkMessage( TEST_ERR_TXT, iniHandleOpenTag ) ;
+    sysRc = 2 ;
+    goto _door ;
+  }
+  checkMessage( TEST_OK_TXT, iniHandleOpenTag ) ;
+  free(iniMem) ;
+#endif
+
+  // -------------------------------------------------------
+  // some test
+  // -------------------------------------------------------
+#if(1)
+  iniReader( "test/cfg/t_reader_002_005.ini", &iniMem ) ;
+  startVal = iniMem ;
+  doPointTest( "value found"         , \
+               RC_NOT_NULL           , \
+               iniHandleValues    , \
+               startVal, endVal, iniCfg, &sysRc   ) ;
+  if( sysRc != 0 )
+  {
+    checkMessage( TEST_ERR_TXT, iniHandleOpenTag ) ;
+    sysRc = 2 ;
+    goto _door ;
+  }
+  checkMessage( TEST_OK_TXT, iniHandleOpenTag ) ;
+  free(iniMem) ;
+
 #endif
 
 _door:
